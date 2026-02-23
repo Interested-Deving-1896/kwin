e14ca69c5faab7acabd9027bf47f636668fd9527
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2026-02-23 16:59:31 +0200

    wayland: Reject output configurations with self-mirroring outputs

--- src/wayland/outputmanagement_v2.cpp
+++ src/wayland/outputmanagement_v2.cpp
@@ -625,6 +625,12 @@ void OutputConfigurationV2Interface::kde_output_configuration_v2_apply(Resource 
         if (!changeset) {
             continue;
         }
+        if (changeset->replicationSource.has_value()) {
+            if (changeset->replicationSource == output->uuid()) {
+                sendFailure(resource, i18n("%1 cannot mirror itself", output->name()));
+                return;
+            }
+        }
         if (changeset->customModes.has_value()) {
             for (const auto &info : *changeset->customModes) {
                 if (info.size.isEmpty()) {
